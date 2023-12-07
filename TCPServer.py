19d8e9ff8efccf55845eb806d1beda9145b16761
CamiloHMS <camilo.santos@icen.ufpa.br>
2023-12-07 12:05:51 -0300

    feat: TCPServer

--- TCPServer.py
+++ TCPServer.py
@@ -1,100 +1,73 @@
+import socket
 from Extractor import Extractor
 import inspect
-import socket
+
+ENDSIZE = 16384
+host = 'localhost'
+port = 8080
+address = (host, port)
+methods = {}
+
+def register_instance(instance):
+    for func_name, function in inspect.getmembers(
+        instance, predicate=inspect.ismethod
+    ):
+        if not func_name.startswith(
+            "__"
+        ) and not func_name.startswith("sup"):
+            methods.update({func_name: function})
+
+def response_handler(resp):
+    send = methods[resp[0]](*resp[1:])
+    return send
 
 
-class Server:
-    def __init__(self, host="localhost", port=8080):
-        self.SENDSIZE = 8192
-        self.host = host
-        self.port = port
-        self.address = (host, port)
-        self.methods = {}
-        self.desc = "Comandos do servidor:\n\nhelp: descrição das utilidades do servidor.\
-            \nf-list: lista de funções.\
-            \ndesc_função: descrição de uma função.\
-            \nfunção_params: executa função."
-        
-    def create_response(self,status_code, body):
-        response = f"HTTP/1.1 {status_code}\r\n"
-        response += "Content-Type: text/html\r\n"
-        response += f"Content-Length: {len(body)}\r\n"
-        response += f"Access-Control-Allow-Origin: *\r\n"
-        response += "\r\n"
-        response += body
-        return response
-
-    def register_instance(self, instance):
-        for func_name, function in inspect.getmembers(
-            instance, predicate=inspect.ismethod
-        ):
-            if not func_name.startswith(
-                "__"
-            ) and not func_name.startswith("sup"):
-                self.methods.update({func_name: function})
-
-    def response_handler(self, resp):
-        # para o retorno de informações extras
-        if resp[0] == "f-list":
-            send = ", ".join(list(self.methods.keys()))
-        elif resp[0] == "desc":
-            send = self.methods[resp[1]].__doc__
-        elif resp[0] == "help":
-            send = self.desc
-        else:
-            # para a utilização das funções
-            send = self.methods[resp[0]](*resp[1:])
-        return send
-
-    def run(self):
-        # criação do socket
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(self.address)
-            s.listen(1)
-
-            # início da conexão
-            print(f"servidor {self.address} ativo.")
-            client_socket, client_address = s.accept()
-            print(f"conexão com: {client_address}")
-            # recebimento da mensagem do cliente
-            while True:
-                #try:
-                data = client_socket.recv(self.SENDSIZE).decode("utf-8")
-
-                if data.startswith("POST"): #start_post = True
-                    resp = data.split("\r\n\r\n", 1)[1].replace('"', '').split('.')
-                    print("//",resp)
-                    send = self.response_handler(resp)
-
-                # Tratamentos de erros com envio de mensagens personalizadas
-                #except IndexError:
-                #    send = "Parâmetros estão faltando"
-                #    print(send)
-                #except KeyError:
-                #    send = "Comando desconhecido"
-                #    print(send)
-                #except (ValueError, TypeError):
-                #    send = (
-                #        "Parâmetros de entrada inválidos ou incompletos"
-                #    )
-                #    print(send)
-                #except (ConnectionAbortedError, KeyboardInterrupt):
-                #    print(f"Servidor {self.address} interrompido")
-                #    s.close()
-                #    break
-
-                # envio da resposta para o socket cliente
-
-                send = self.create_response(200, str(send))
-                client_socket.sendall(str(send).encode("utf-8"))
-
+def create_response(status_code, body):
+  response = f"HTTP/1.1 {status_code}\r\n"
+  response += "Content-Type: text/html\r\n"
+  response += f"Content-Length: {len(body)}\r\n"
+  response += f"Access-Control-Allow-Origin: *\r\n"
+  response += "\r\n"
+  response += body
+  return response
 
 def main():
-    server = Server()
-    inst = Extractor()
-    server.register_instance(inst)
-    server.run()
+  inst = Extractor()
+  register_instance(inst)
+
+  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server_socket.bind((host, port))
+  server_socket.listen(1)
+
+  print(f"Servidor HTTP rodando em http://{host}:{port}")
+
+  while True:
+      client_socket, client_address = server_socket.accept()
+      print(f"Conexão recebida de {client_address[0]}:{client_address[1]}")
+
+      data = client_socket.recv(ENDSIZE).decode('utf-8')
+      print(f"Recebido:\n{data}")
+
+      # Verifica se a requisição é um POST
+      if data.startswith("POST"):
+          # Obtém o corpo da requisição POST
+          body = data.split("\r\n\r\n", 1)[1].replace('"', '')
+          
+          # Exibe o corpo da requisição no console
+          print(f"Conteúdo da requisição POST:\n{body}")
+          msg = response_handler(body.split('.'))
+          
+          # Aqui você pode processar o corpo da requisição POST como desejado
+          # Por exemplo, você pode salvar os dados em um arquivo de log ou banco de dados
+
+          # Para uma resposta simples, você pode retornar o corpo de volta
+          # Para outras requisições, retorne uma resposta padrão
+      response = create_response(200, str(msg))
+
+      client_socket.send(response.encode('utf-8'))
+      client_socket.close()
+
 
 
 if __name__ == "__main__":
-    main()
+  main()
---
+++ TCPServerTest.py
@@ -0,0 +1,100 @@
+from Extractor import Extractor
+import inspect
+import socket
+
+
+class Server:
+    def __init__(self, host="localhost", port=8080):
+        self.SENDSIZE = 8192
+        self.host = host
+        self.port = port
+        self.address = (host, port)
+        self.methods = {}
+        self.desc = "Comandos do servidor:\n\nhelp: descrição das utilidades do servidor.\
+            \nf-list: lista de funções.\
+            \ndesc_função: descrição de uma função.\
+            \nfunção_params: executa função."
+        
+    def create_response(self,status_code, body):
+        response = f"HTTP/1.1 {status_code}\r\n"
+        response += "Content-Type: text/html\r\n"
+        response += f"Content-Length: {len(body)}\r\n"
+        response += f"Access-Control-Allow-Origin: *\r\n"
+        response += "\r\n"
+        response += body
+        return response
+
+    def register_instance(self, instance):
+        for func_name, function in inspect.getmembers(
+            instance, predicate=inspect.ismethod
+        ):
+            if not func_name.startswith(
+                "__"
+            ) and not func_name.startswith("sup"):
+                self.methods.update({func_name: function})
+
+    def response_handler(self, resp):
+        # para o retorno de informações extras
+        if resp[0] == "f-list":
+            send = ", ".join(list(self.methods.keys()))
+        elif resp[0] == "desc":
+            send = self.methods[resp[1]].__doc__
+        elif resp[0] == "help":
+            send = self.desc
+        else:
+            # para a utilização das funções
+            send = self.methods[resp[0]](*resp[1:])
+        return send
+
+    def run(self):
+        # criação do socket
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(self.address)
+            s.listen(1)
+
+            # início da conexão
+            print(f"servidor {self.address} ativo.")
+            client_socket, client_address = s.accept()
+            print(f"conexão com: {client_address}")
+            # recebimento da mensagem do cliente
+            while True:
+                #try:
+                data = client_socket.recv(self.SENDSIZE).decode("utf-8")
+
+                if data.startswith("POST"): #start_post = True
+                    resp = data.split("\r\n\r\n", 1)[1].replace('"', '').split('.')
+                    print("//",resp)
+                    send = self.response_handler(resp)
+
+                # Tratamentos de erros com envio de mensagens personalizadas
+                #except IndexError:
+                #    send = "Parâmetros estão faltando"
+                #    print(send)
+                #except KeyError:
+                #    send = "Comando desconhecido"
+                #    print(send)
+                #except (ValueError, TypeError):
+                #    send = (
+                #        "Parâmetros de entrada inválidos ou incompletos"
+                #    )
+                #    print(send)
+                #except (ConnectionAbortedError, KeyboardInterrupt):
+                #    print(f"Servidor {self.address} interrompido")
+                #    s.close()
+                #    break
+
+                # envio da resposta para o socket cliente
+
+                send = self.create_response(200, str(send))
+                client_socket.sendall(str(send).encode("utf-8"))
+
+
+def main():
+    server = Server()
+    inst = Extractor()
+    server.register_instance(inst)
+    server.run()
+
+
+if __name__ == "__main__":
+    main()
